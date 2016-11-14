6adc5bf2c093e2a06715a62b708a4d55a00c377c
Abhinav Kumar Kushwaha <abhinav@Abhinav1-INL.local>
2016-11-14 17:07:33 +0530

    testing emoji in git message :rocket:

--- designpatterns/script.js
+++ designpatterns/script.js
@@ -1,7 +1,7 @@
 require(['cookie_module', 'xhr_with_xsrf_module', 'random_module'], function (cookie, xhr, rand) {
     cookie.create('x-xsrf-token', 'asdasd');
     xhr.setXsrfToken('x-xsrf-token');
-    xhr.get('data.json?_=' + rand.number(5)).then(function (data) {
+    xhr.get('data.json?_=' + rand.number(6)).then(function (data) {
         console.log(data.success);
     });
 });
